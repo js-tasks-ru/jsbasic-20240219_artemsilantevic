d911f5ad205fddddf7a7702bd3ea3bd7b50a1805
xanexii <artemsilantevic@gmail.com>
2024-03-07 16:48:37 +0300

    4 и 5 задание

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,5 @@
 function showSalary(users, age) {
-  // ваш код...
+  const filteredUsers = users.filter(user => user.age <= age);
+  const userSalaries = filteredUsers.map(user => `${user.name}, ${user.balance}`);
+  return userSalaries.join('\n');
 }
